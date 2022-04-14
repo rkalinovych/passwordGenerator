55051031c7d67220764987b55a1cfa625e70a00d
Rostyslav Kalinovych <rostyslav_k@optimove.com>
2022-04-14 14:58:00 +0300

    pw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  public isUseLetters = false;
+  public isUseNumbers = false;
+  public isUseSymbols = false;
+  public password="";
+  public length = 0;
+
+  onChangeLenght(event: Event){
+    var parsedValue = parseInt((event.target as HTMLInputElement).value);
+    if(parsedValue !=NaN){
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLeters(){
+    this.isUseLetters = !this.isUseLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.isUseNumbers = !this.isUseNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.isUseSymbols = !this.isUseSymbols;
+  }
+
+  onButtonClick(){
+    const numbers ='1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    var validChars ='';
+    if(this.isUseLetters){
+      validChars += letters;
+    }
+    if(this.isUseNumbers){
+      validChars += numbers;
+    }
+    if(this.isUseSymbols){
+      validChars += symbols;
+    }
+    var generatedPassword='';
+    for(var i=0; i < this.length; i++){
+      var index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index]; 
+    }
+    this.password = generatedPassword
+  }
 }
